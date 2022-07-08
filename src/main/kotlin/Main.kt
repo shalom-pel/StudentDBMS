e3c48eff05f719907962d64a39c19371d405f764
SVPH\shalom.pel <shalom.pel@softvision.com>
2022-07-08 18:09:03 +0800

    change if statement to when - main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,23 +13,32 @@ fun displayMainOptions() {
     println("\t[D] - Display")
     println("\t[E] - Exit App")
     print("\nSelection: ")
-    val selection = readln().lowercase()
+    val selection = readln().uppercase()
 
     val actions = SdbmsActions()
 
-    if (selection == "A") {
-        actions.addStudent()
-    } else if (selection == "B") {
-        actions.editStudent()
-    } else if (selection == "C") {
-        actions.deleteStudent()
-    } else if (selection == "D") {
-        actions.displayStudent()
-    } else if (selection == "E") {
-        //
-    } else {
-        println("Invalid Selection. Please try again")
-        displayMainOptions()
+//    if (selection == "A") {
+//        actions.addStudent()
+//    } else if (selection == "B") {
+//        actions.editStudent()
+//    } else if (selection == "C") {
+//        actions.deleteStudent()
+//    } else if (selection == "D") {
+//        actions.displayStudent()
+//    } else if (selection == "E") {
+//        //
+//    } else {
+//        println("Invalid Selection. Please try again")
+//        displayMainOptions()
+//    }
+
+    when (selection) {
+        "A" -> println(actions.addStudent())
+        "B" -> println(actions.editStudent())
+        "C" -> println(actions.deleteStudent())
+        "D" -> println(actions.displayStudent())
+        "E" -> println("Exit App")
+        else -> println("Invalid Selection. Please try again")
     }
 }
 
